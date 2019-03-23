cada5bfc8749346492a1cdaf5e763440894f9175
Sergio225 <s3rg10.martinez.roman@gmail.com>
2019-03-23 18:23:15 +0100

    Already functional Architecture.

--- src/main/java/com/ragnarok/entities/Game.java
+++ src/main/java/com/ragnarok/entities/Game.java
@@ -1,8 +1,33 @@
 package com.ragnarok.entities;
 
+import javax.inject.Named;
+
+
+@Named
 public class Game {
 
-	public int yearsOld;
-	public String name;
+	private int yearsOld = 1;
+	private String name = "APEX";
+	
+	public int getYearsOld() {
+		return yearsOld;
+	}
+
+	public void setYearsOld(int yearsOld) {
+		this.yearsOld = yearsOld;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String showName()
+	{
+		return "That's the name of the game: " + this.name;
+	}
 	
 }
